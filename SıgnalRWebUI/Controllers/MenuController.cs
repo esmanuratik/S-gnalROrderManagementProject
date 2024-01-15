8c2cbe00a8ada63cdc92b3e16c233a4d97ea4f5b
esmanuratik <atikesmanur@gmail.com>
2024-01-15 09:07:48 +0300

    Menu işlemleri ve Ajax

---
+++ SıgnalRWebUI/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SıgnalRWebUI.DTOs.ProductDTOs;
+
+namespace SıgnalRWebUI.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7001/api/Products");
+            var  jsonData=await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+
+            return View(values);
+        }
+    }
+}
